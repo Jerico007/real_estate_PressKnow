f028cf9028e5525c79f5f550dd9d06495b67c291
Jerico007 <jericodcosta95375788@gmail.com>
2025-01-30 15:20:21 +0530

    second commit

---
+++ src/components/HeaderMenu.vue
@@ -0,0 +1,49 @@
+<script setup>
+import { ref } from 'vue';
+
+import logo from "@/components/icons/logo.svg";
+
+
+const menuState = ref({
+    drawerStatus: false,
+    listContent: ['Home', 'About', 'Services', 'Properties', 'Contact']
+});
+
+function toggleMenu() {
+    menuState.value.drawerStatus = !menuState.value.drawerStatus;
+}
+</script>
+
+<template>
+    <div class="navbar container-fluid d-flex justify-content-between align-items-center p-2 ">
+        <!-- App logo -->
+        <div class="app-logo">
+            <img :src="logo" id="nav-logo" alt="No Logo">
+        </div>
+
+        <!-- Menu items -->
+        <div class="d-none d-md-flex justify-content-around g-2">
+            <li v-for="x in menuState.listContent" class="menu-list mx-2">{{ x }}</li>
+        </div>
+
+
+        <!-- Navigation List (Slide from Right to Left) -->
+        <ul :class="['list-content', { 'show-menu': menuState.drawerStatus }]">
+            <li v-for="x in menuState.listContent" class="menu-list-phone mx-2">{{ x }}</li>
+        </ul>
+
+        <!-- Social media icons -->
+        <div class="social-icon-holder d-none d-md-flex ">
+            <font-awesome-icon class="nav-icons" :icon="['fab', 'twitter']" style="cursor: pointer;" />
+            <font-awesome-icon class="nav-icons" :icon="['fab', 'pinterest']" style="cursor: pointer;" />
+            <font-awesome-icon class="nav-icons" :icon="['fab', 'youtube']" style="cursor: pointer;" />
+        </div>
+
+        <!-- Button to Mobile responsiveness -->
+        <button class="menu-toggle d-md-none" @click="toggleMenu">
+            <font-awesome-icon v-show="!menuState.drawerStatus" class="hamburger" :icon="['fas', 'bars']" size="lg"  />
+            <font-awesome-icon v-show="menuState.drawerStatus" class="hamburger" :icon="['fas', 'xmark']"  size="lg"/>
+        </button>
+        
+    </div>
+</template>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
-import './assets/main.css'
 
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+// import router from './router'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+// Import Bootstrap JS (Optional, for components like modals, tooltips, etc.)
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+
+import './assets/main.css'
+// Import FontAwesome core
+import { library } from '@fortawesome/fontawesome-svg-core';
+// Import specific icons
+import { faUser, faHome, faBars ,faXmark} from '@fortawesome/free-solid-svg-icons';
+
+import { faTwitter, faPinterest, faYoutube } from '@fortawesome/free-brands-svg-icons';
+// Import FontAwesome Vue component
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+// Add icons to the library
+library.add(faUser, faHome, faTwitter, faPinterest, faYoutube, faBars,faXmark);
 
 const app = createApp(App)
-
-app.use(router)
+app.component('font-awesome-icon', FontAwesomeIcon); // Register component globally
 
 app.mount('#app')
